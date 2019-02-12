de904479f5fde9487f97bd4347756f251eb7fb8a
Ben Hale <bhale@pivotal.io>
2019-02-12 11:41:32 -0800

    Release Script Updates
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -5,14 +5,18 @@ set -e -u
 RELEASE=$1
 SNAPSHOT=$2
 
+./mvnw versions:update-properties -DgenerateBackupPoms=false
+git add .
+git commit --message "Subproject Updates"
+
 git branch -f release
 git checkout release
 ./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
-git commit --message "v$RELEASE Release"
-git tag -s v$RELEASE -m "v$RELEASE"
+git commit --message "$RELEASE Release"
+git tag -s $RELEASE -m "$RELEASE"
 
 git master
 ./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
 git add .
-git commit --message "v$SNAPSHOT Development"
+git commit --message "$SNAPSHOT Development"
