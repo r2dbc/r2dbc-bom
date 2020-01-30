c80bb3cc10cf0d3b7b5a9bee3cbb0fa42919aca0
Greg Turnquist <gturnquist@pivotal.io>
2020-01-30 13:37:09 -0600

    Add Jenkins job for BOM.
    [resolves #24]

--- ci/build.sh
+++ ci/build.sh
@@ -1,13 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -euo pipefail
 
-[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
-
-r2dbc_bom_artifactory=$(pwd)/r2dbc-bom-artifactory
-
-rm -rf $HOME/.m2/repository/io/r2dbc 2> /dev/null || :
-
-cd r2dbc-bom
-./mvnw deploy \
-    -DaltDeploymentRepository=distribution::default::file://${r2dbc_bom_artifactory}
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/r2dbc-maven-repository" ./mvnw -P${PROFILE} -Dmaven.test.skip=true clean deploy -B
